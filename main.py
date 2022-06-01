285ab5bed2c13f05ef34e1b1406e46e5abeb14dc
Omolola Odunowo <omololaodunowo@gmail.com>
2022-06-01 12:21:30 +0100

    updated main.py and added updated answers to repo

--- main.py
+++ main.py
@@ -16,11 +16,20 @@ print(read_file_content('filename'))
 
 
 def count_words():
-    words=[]
-    text = read_file_content("story.txt")
+    text = read_file_content('story.txt')
     # [assignment] Add your code here
-    
+    counts=dict()
     words = text.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] +=1
+        else:
+            counts[word]=1
+
+    return counts
+print(count_words())
+
     
 
 
